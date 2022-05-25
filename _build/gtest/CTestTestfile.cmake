a610ff6e5645496e642a93ccb0e70f2b1b82dfd9
marginoon <g-l-o-120404@rambler.ru>
2022-05-25 14:58:00 -0400

    delete _build

--- _build/gtest/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/kali/Documents/rk_timp/third-party/gtest
-# Build directory: /home/kali/Documents/rk_timp/_build/gtest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
--- _build/gtest/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/kali/Documents/rk_timp/third-party/gtest/googletest
-# Build directory: /home/kali/Documents/rk_timp/_build/gtest/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
