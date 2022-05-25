7ef418426d738fb22b35db4dbbaee65eecde2170
marginoon <g-l-o-120404@rambler.ru>
2022-05-25 14:57:24 -0400

    added all

---
+++ _build/CMakeFiles/sort.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sort.dir/timp.cpp.o"
+  "CMakeFiles/sort.dir/timp.cpp.o.d"
+  "libsort.a"
+  "libsort.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/sort.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ _build/gtest/googletest/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/kali/Documents/rk_timp/third-party/gtest/googletest
+# Build directory: /home/kali/Documents/rk_timp/_build/gtest/googletest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
